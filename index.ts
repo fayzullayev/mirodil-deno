c288e7fba2a9c95ddec759dcf406d7f877fe28c0
Mirodil Fayzullayev <odilfayz22@gmail.com>
2021-06-23 16:11:34 +0500

    main

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from 'https://deno.land/std@0.99.0/http/server.ts';
-const server = serve({ port: 8000 });
-console.log('http://localhost:8000/');
-
-for await (const req of server) {
-  req.respond({ body: 'Mirodil Hello World\n' });
-}

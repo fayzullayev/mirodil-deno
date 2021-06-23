f86176eb2db14083847b897476347d6f992c020f
Mirodil Fayzullayev <odilfayz22@gmail.com>
2021-06-23 16:28:12 +0500

    qwertyuio

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 addEventListener('fetch', (event) => {
-  event.respondWith(new Response('Hello 26 World!!!'));
+  event.respondWith(
+    new Response('Helf erfer4g er4ger4 g5er5g5 er4lo 26 World!!!')
+  );
 });
